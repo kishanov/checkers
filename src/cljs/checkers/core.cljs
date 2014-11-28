78d989f029f06bfaa38caebf04a0d9ac4a87ec5d
Kirill Ishanov <kirill.ishanov@gmail.com>
2014-11-28 15:23:19 -0800

    initial checkers placement

--- src/cljs/checkers/core.cljs
+++ src/cljs/checkers/core.cljs
@@ -7,8 +7,6 @@
 
 ;; -------------------------
 ;; State
-(defonce app-state (atom {:text "Hello, this is patrick: "}))
-
 
 (def empty-board (->> (interleave (repeat 32 {:color :white})
                                   (repeat 32 {:color :black}))
@@ -18,27 +16,55 @@
                       (map vec)
                       vec))
 
+
+
+
+(defn initial-checkers-placement
+  [board]
+  (let [place-checkers (fn [board start-row-number end-row-number color]
+                         (let [cell-coords (for [i (range start-row-number (inc end-row-number))
+                                                 j (range 8)]
+                                             [i j])]
+
+                           (reduce (fn [result-board coord]
+                                     (let [cell-color (get-in result-board (conj coord :color))]
+                                       (if (= cell-color :black)
+                                         (assoc-in result-board (conj coord :checker) {:color color})
+                                         result-board)))
+                                   board cell-coords)))]
+
+    (place-checkers (place-checkers board 0 2 :white) 5 7 :black)))
+
+
+
+
+
+(def board-state (atom (initial-checkers-placement empty-board)))
+
 (defn board
   [board-data]
   [:div.board
    (for [i (range 8)]
-     [:div.board-row
+     [:div.board-row {:key (str "row-" i)}
       (for [j (range 8)]
-        [:div.board-cell
+        (let [cell (get-in board-data [i j])]
+          [:div.board-cell {:class (if (= (cell :color) :white)
+                                     "board-cell-white"
+                                     "board-cell-black")
+                            :key   (str "cell-" i "-" j)}
 
-         {:class (if (= (get-in board-data [i j :color]) :white)
-                   "board-cell-white"
-                   "board-cell-black")}]
-
-        )
-
-      ])
-   ])
+           (when (contains? cell :checker)
+             [:div.checker
+              {:class (if (= (get-in cell [:checker :color]) :white)
+                        "checker-white"
+                        "checker-black")}
+              ])
+           ]))])])
 
 
 
 ;; -------------------------
 ;; Initialize app
 (defn init! []
-  (reagent/render-component [board empty-board] (.getElementById js/document "app")))
+  (reagent/render-component [board @board-state] (.getElementById js/document "app")))
 
