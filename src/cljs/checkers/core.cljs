6ab244db9b4428f3c31f1b384cb955bd9fa600c4
Kirill Ishanov <kirill.ishanov@gmail.com>
2014-11-29 10:22:17 -0800

    make-a-move function (naive, no validation)

--- src/cljs/checkers/core.cljs
+++ src/cljs/checkers/core.cljs
@@ -17,8 +17,6 @@
                       vec))
 
 
-
-
 (defn initial-checkers-placement
   [board]
   (let [place-checkers (fn [board start-row-number end-row-number color]
@@ -36,35 +34,52 @@
     (place-checkers (place-checkers board 0 2 :white) 5 7 :black)))
 
 
-
-
-
 (def board-state (atom (initial-checkers-placement empty-board)))
 
-(defn board
-  [board-data]
-  [:div.board
-   (for [i (range 8)]
-     [:div.board-row {:key (str "row-" i)}
-      (for [j (range 8)]
-        (let [cell (get-in board-data [i j])]
-          [:div.board-cell {:class (if (= (cell :color) :white)
-                                     "board-cell-white"
-                                     "board-cell-black")
-                            :key   (str "cell-" i "-" j)}
+(def selected-checker (atom nil))
+(def src-coord (atom nil))
+(def dst-coord (atom nil))
 
-           (when (contains? cell :checker)
-             [:div.checker
-              {:class (if (= (get-in cell [:checker :color]) :white)
-                        "checker-white"
-                        "checker-black")}
-              ])
-           ]))])])
+
+(defn make-a-move
+  []
+  (swap! board-state update-in @src-coord dissoc :checker)
+  (swap! board-state assoc-in (conj @dst-coord :checker) @selected-checker))
+
+
+
+(defn board
+  [state]
+  (fn []
+    (let [current-board-state @state]
+      [:div.board
+       (for [i (range 8)]
+         [:div.board-row {:key (str "row-" i)}
+          (for [j (range 8)]
+            (let [cell (get-in current-board-state [i j])]
+              [:div.board-cell
+               {:class    (if (= (cell :color) :white)
+                            "board-cell-white"
+                            "board-cell-black")
+                :key      (str "cell-" i "-" j)
+                :on-click #(when-not (contains? cell :checker)
+                            (reset! dst-coord [i j])
+                            (make-a-move))}
+
+               (when (contains? cell :checker)
+                 [:div.checker
+                  {:class    (if (= (get-in cell [:checker :color]) :white)
+                               "checker-white"
+                               "checker-black")
+                   :on-click #(do
+                               (reset! selected-checker (cell :checker))
+                               (reset! src-coord [i j]))
+                  }
+                  ])]))])])))
 
 
 
 ;; -------------------------
 ;; Initialize app
-(defn init! []
-  (reagent/render-component [board @board-state] (.getElementById js/document "app")))
+(reagent/render-component [board board-state] (.getElementById js/document "app"))
 
